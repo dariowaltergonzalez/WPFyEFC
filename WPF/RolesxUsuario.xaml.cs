18843b18e0e22be19c16edce33a2fdcf20d88210
Dario Walter <dariogonzalez08@gmail.com>
2024-04-12 09:32:22 -0300

    obtener los items seleccionados de un listview

--- WPF/RolesxUsuario.xaml.cs
+++ WPF/RolesxUsuario.xaml.cs
@@ -74,57 +74,23 @@ namespace WPF
             //lstRolesAsignados.ItemsSource = ListaRolesPorUsuario;
         }
 
-        //x:5
-        //foreach (var item in lstRoles.SelectedItems.OfType<rolesBox>())
-        //    {
-        //        MessageBox.Show(item.Id.ToString());
-        //        MessageBox.Show(item.Nombre);
-        //    }
-
-
+          
         private void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
-            //var currentItemText = lstRoles.SelectedValue.ToString();
-
-            
-            //var currentItemIndex = lstRoles.SelectedIndex;
-
-            //MessageBox.Show(currentItemText);
-
-            foreach (var item in lstRoles.SelectedItems.OfType<rolesBox>())
+            foreach (var item in lstRoles.SelectedItems)
             {
-                MessageBox.Show(item.Id.ToString());
+                var rol = (rolesBox)item;
+                MessageBox.Show($"Id:{rol.Id} Nombre: {rol.Nombre}");
             }
-
-            //foreach (var item in lstRoles.SelectedValuePath.che)
-            //{
-            //    MessageBox.Show("ddd");
-            //}
-
-            //foreach (var item in lstRoles.Items.OfType<rolesBox>())
-            //{
-
-            //    MessageBox.Show(item.Nombre);
-            //}
-
-            //var chapters = new rolesBox();
-
-            //foreach (var item in lstRoles.SelectedItems)
-            //{
-            //    MessageBox.Show("ddd");
-            //}
-            //for (int i = 0; i < lstRoles.Items.Count; i++)
-            //{
-            //    if (lstRoles.Items[i].Checked)
-            //    {
-            //        MessageBox.Show("Listview items " + lstRoles.Items[i] + " is checked");
-            //    }
-            //}
         }
 
         private void btnSacar_Click(object sender, RoutedEventArgs e)
         {
-
+            foreach (var item in lstRoles2.SelectedItems)
+            {
+                var rol = (rolesBox)item;
+                MessageBox.Show(rol.Nombre);
+            }
         }
 
     }
